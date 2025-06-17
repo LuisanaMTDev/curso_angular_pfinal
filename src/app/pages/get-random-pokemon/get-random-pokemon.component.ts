924725db6c8950c3341e942058ed04f74d79b1eb
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2025-06-17 16:24:24 -0400

    v4

--- src/app/pages/get-random-pokemon/get-random-pokemon.component.ts
+++ src/app/pages/get-random-pokemon/get-random-pokemon.component.ts
@@ -4,41 +4,43 @@ import { HomeButtonComponent } from "../../components/home-button/home-button.co
 import { LogoutComponent } from "../../components/logout/logout.component";
 
 interface AbilityDetails {
-    name: string,
-    url: string
+  name: string,
+  url: string
 }
 
 interface Ability {
-    ability: AbilityDetails,
-    is_hidden: boolean,
-    slot: number
+  ability: AbilityDetails,
+  is_hidden: boolean,
+  slot: number
 }
 
 @Component({
-    selector: 'app-get-random-pokemon',
-    imports: [HomeButtonComponent, LogoutComponent],
-    templateUrl: './get-random-pokemon.component.html',
-    styleUrl: './get-random-pokemon.component.css'
+  selector: 'app-get-random-pokemon',
+  imports: [HomeButtonComponent, LogoutComponent],
+  templateUrl: './get-random-pokemon.component.html',
+  styleUrl: './get-random-pokemon.component.css'
 })
 export class GetRandomPokemonComponent {
-    constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService) { }
 
-    name: string = ''
-    officialArt: string = ''
-    altForOfficialArt: string = `Arte oficial de ${this.name}`
-    abilities: Ability[] = []
+  name: string = ''
+  officialArt: string = ''
+  altForOfficialArt: string = `Arte oficial de ${this.name}`
+  abilities: Ability[] = []
+  type: string = ''
 
-    getRandomPokemon() {
-        this.pokemonService.getRandomPokemon().subscribe(data => {
-            console.log(data);
+  getRandomPokemon() {
+    this.pokemonService.getRandomPokemon().subscribe(data => {
+      console.log(data);
 
-            this.name = data.name
-            this.officialArt = data.sprites.front_default
-            this.abilities = data.abilities
-        })
-    }
+      this.name = data.name
+      this.officialArt = data.sprites.front_default
+      this.abilities = data.abilities
+      this.type = data.types[0].type.name
+    })
+  }
 
-    ngOnInit() {
-        this.getRandomPokemon()
-    }
+  ngOnInit() {
+    this.getRandomPokemon()
+  }
 }
--- src/app/pages/search-pokemon-by-name/search-pokemon-by-name.component.ts
+++ src/app/pages/search-pokemon-by-name/search-pokemon-by-name.component.ts
@@ -4,6 +4,17 @@ import { PokemonService } from '../../services/pokemon.service';
 import { HomeButtonComponent } from "../../components/home-button/home-button.component";
 import { LogoutComponent } from "../../components/logout/logout.component";
 
+interface AbilityDetails {
+    name: string,
+    url: string
+}
+
+interface Ability {
+    ability: AbilityDetails,
+    is_hidden: boolean,
+    slot: number
+}
+
 @Component({
     selector: 'app-search-pokemon-by-name',
     imports: [FormsModule, HomeButtonComponent, LogoutComponent],
@@ -13,13 +24,19 @@ import { LogoutComponent } from "../../components/logout/logout.component";
 export class SearchPokemonByNameComponent {
     constructor(private pokemonService: PokemonService) { }
     name: string = ''
+    altForOfficialArt: string = ''
     pokemonOfficialArt: string = ''
     pokemonName: string = ''
+    abilities: Ability[] = []
+    type: string = ''
 
     getPokemon() {
-        this.pokemonService.getPokemonByName(this.name).subscribe(data => {
+        this.pokemonService.getPokemonByName(this.name.toLowerCase()).subscribe(data => {
             this.pokemonName = data.name
+            this.altForOfficialArt = `Arte oficial de ${this.pokemonName}`
             this.pokemonOfficialArt = data.sprites.front_default
+            this.abilities = data.abilities
+            this.type = data.types[0].type.name
         })
     }
 }
